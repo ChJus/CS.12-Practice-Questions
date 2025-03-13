051ca1574e76e2d2e0fc8497624d3c02acb854b8
ChJus <92497757+ChJus@users.noreply.github.com>
2025-03-13 08:02:23 +0800

    updates

---
+++ src/March12.java
@@ -0,0 +1,26 @@
+/*
+swap(A, B):
+  TEMP = A
+  B = TEMP
+  A = B
+
+(a.i) A = 3, B = 5; after swap function called, A = 3, B = 3;
+(a.ii) Swap the second and third line. So:
+       TEMP = A
+       A = B
+       B = TEMP
+
+(b)
+    swapRows(MAT, K, L):
+        TEMP = MAT[K]
+        MAT[K] = MAT[L]
+        MAT[L] = TEMP
+
+
+ */
+
+public class March12 {
+  public static void main(String[] args) {
+
+  }
+}
